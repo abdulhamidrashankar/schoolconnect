82113ebfc32e2e88411aaf90d98cab6bbb26a42e
Anfal14 <adilanfal141431@gmail.com>
2025-07-29 11:41:38 +0530

    Added DML queries

---
+++ database/DQL queries.sql
@@ -0,0 +1,13 @@
+SELECT * FROM role;
+SELECT * FROM users;
+SELECT * FROM superadmin;
+SELECT * FROM admin;
+SELECT * FROM student;
+SELECT * FROM teacher;
+
+Drop table superadmin;
+Drop table admin;
+Drop table student ;
+Drop table teacher;
+Drop table users;
+Drop table role;
